d5b6c2c1b1f13ccbb195a6d82b2ca8d1e45acaab
DaadAmar <115740830+DaadAmar@users.noreply.github.com>
2024-03-03 13:51:55 +0200

    Add files via upload

---
+++ CS112_A1_T2_1_20230779.py
@@ -0,0 +1,70 @@
+# File: CS112_A1_T2_1_20230779.py
+# program: A two player game in python where the goal
+#         is to reach 100. Each player chooses from 0-10.
+#         The first player to reach 100 wins.
+# Author: Daad Amar Osman
+# ID: 20230779
+
+def main():
+    # Welcome Message for the players
+    print("\n                               The 100 Game ")
+    print('''     
+          A two players game that starts from 0 and the goal is to 
+          reach the sum of 100, each player alternatively adds a number 
+          from 1 to 10 to the sum.The player who reaches 100 first wins.
+    ''')
+    # The starting number and the target number
+    Sum = 0
+    Goal = 100
+
+    # The Game 
+    while Sum < Goal:
+        #Player 1 entry and validation
+        play =int(input("Player 1: Enter a number from 1 - 10: "))
+        while play < 1 or play > 10:
+            play = int(input("Player 1: Enter a number from 1 - 10: "))
+            
+        # Check if the sum is above the required Goal
+        temp = Sum + play    
+        while  temp > Goal :
+            print(f"The sum must not exceed {Goal} \n")
+            play = int (input("Player 1: Enter a number from 1 - 10: "))
+            temp = Sum + play
+        Sum += play
+        print(f"The Total is now {Sum}\n")
+        
+        # Check for Winner
+        if Sum >= Goal:
+            print("Player 1 Is the Winner 🏆\nGame Over \n")
+            break
+        
+        #Player 2 entry and validation
+        play =int(input("Player 2: Enter a number from 1 - 10: "))
+        while play < 1 or play > 10:
+            play = int(input("Player 2: Enter a number from 1 - 10: "))
+            
+        # Check if the sum is above the required Goal
+        temp = Sum + play    
+        while  temp > Goal :
+            print(f"The sum must not exceed {Goal} \n")
+            play = int (input("Player 2: Enter a number from 1 - 10: "))
+            temp = Sum + play
+        Sum += play
+        print(f"The Total is now {Sum}\n")
+        
+        # Check for Winner
+        if Sum >= Goal:
+            print("Player 2 Is the Winner 🏆\nGame Over \n")
+            break
+        
+#set the intial status to yes        
+game = 'yes'
+
+#repeat playing the game until told otherwise
+while game == 'yes':
+    main()
+    game = input("Do you want to play The 100 Game again ?(yes/no)  ")
+    
+
+       
+
