394030ac702bcdc6e264d53d266958d6bc6fa47e
DaadAmar <115740830+DaadAmar@users.noreply.github.com>
2024-03-05 12:29:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main() {
+    home:
+//         Welcome Message for the players
+    cout << "\n                               The 100 Game \n\n";
+    cout << ""
+            "          A two players game that starts from 0 and the goal is to \n"
+            "          reach the sum of 100, each player alternatively adds a number \n"
+            "          from 1 to 10 to the sum.The player who reaches 100 first wins.\n\n";
+
+// The starting number and the target number
+
+    int sum = 0, goal = 100, play;
+
+// The Game
+
+    while (sum < goal) {
+
+        //Player 1 entry and validation
+        cout << "\nPlayer 1: Enter a number from 1 - 10: ";
+        cin >> play;
+        while (play < 1 or play > 10) {
+            cout << "Player 1: Enter a number from 1 - 10: ";
+            cin >> play;
+        }
+        // Check if the sum is above the required Goal
+        int temp = sum + play;
+        while (temp > goal) {
+            cout << "The sum must not exceed " << goal << "\n";
+            cout << "Player 1: Enter a number from 1 - 10: ";
+            cin >> play;
+            temp = sum + play;
+        }
+
+        sum = sum + play;
+        cout << "The Total is now " << sum << endl;
+        if (sum >= goal) {
+            cout << "\nPlayer 1 Is the Winner!!! \nGame Over \n";
+            break;
+        } else {
+            cout << "";
+        }
+
+        //Player 2 entry and validation
+        cout << "\nPlayer 2: Enter a number from 1 - 10: ";
+        cin >> play;
+        while (play < 1 or play > 10) {
+            cout << "Player 2: Enter a number from 1 - 10: ";
+            cin >> play;
+        }
+        temp = sum + play;
+        while (temp > goal) {
+            cout << "The sum must not exceed " << goal << "\n";
+            cout << "Player 2: Enter a number from 1 - 10: ";
+            cin >> play;
+            temp = sum + play;
+        }
+        sum += play;
+        cout << "The Total is now " << sum << endl;
+        if (sum >= goal) {
+            cout << "\nPlayer 2 Is the Winner!!! \nGame Over \n";
+            break;
+        } else {
+            cout << "";
+        }
+    }
+    int status;
+    cout << "\nDo you want to play The 100 Game again ?(1 for yes/ 0 for no)   ";
+    cin >> status;
+    if (status != 1) {
+        cout << "\nGoodbye ";
+
+    }
+    else
+    {
+        goto home;
+    }
+
+
+    return 0;
+}
+
